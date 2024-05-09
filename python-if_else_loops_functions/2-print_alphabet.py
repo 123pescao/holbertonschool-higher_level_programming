dffb0862b6b04b2b5c09676a15c3aa0826e3b19f
Joel Caban <joel.caban2017@gmail.com>
2024-05-08 21:24:59 -0400

    adding end= to print function

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-print("".join("{:c}".format(letter) for letter in range(ord('a'), ord('z') + 1)))
+print("".join("{:c}".format(letter) for letter in range(ord('a'), ord('z') + 1)), end="")
 
