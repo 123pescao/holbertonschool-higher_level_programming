7641d5eacd5c2dd934e6da645350536da17af427
Joel Caban <joel.caban2017@gmail.com>
2024-05-13 12:13:49 -0400

    Init commit

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+    """
+    Retrieves an element from a list safely.
+
+    Args:
+        my_list (list): The list from which to retrieve the element.
+        idx (int): The index of the element to retrieve.
+
+    Returns:
+        The element at the specified index if within valid range, None otherwise.
+    """
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
+
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
