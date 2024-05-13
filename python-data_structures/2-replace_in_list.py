0e9b252dbf25d51a80f2ea72569db1f6d45c3906
Joel Caban <joel.caban2017@gmail.com>
2024-05-13 12:25:25 -0400

    fixing cmment

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,17 +2,6 @@
 
 
 def replace_in_list(my_list, idx, element):
-    """
-    Replaces an element in a list at a specific position if the index is valid.
-
-    Args:
-        my_list (list): The list to modify.
-        idx (int): The index at which to replace an element.
-        element (Any): The new element to insert at the specified index.
-
-    Returns:
-        list: The modified list if the index is valid, otherwise the original list.
-    """
     if 0 <= idx < len(my_list):
         my_list[idx] = element
     return my_list
