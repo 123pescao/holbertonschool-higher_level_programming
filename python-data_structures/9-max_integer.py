93b27384c71dbd10e33c5f5bedf008e9b90a2a23
Joel Caban <joel.caban2017@gmail.com>
2024-05-13 15:53:01 -0400

    init commit

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def max_integer(my_list=[]):
        if not my_list:
         return None
@@ -9,6 +10,7 @@ def max_integer(my_list=[]):
         if number > max_value:
             max_value = number
     return max_value
+
 
 if __name__ == "__main__":
     my_list = [1, 90, 2, 13, 34, 5, -13, 3]
