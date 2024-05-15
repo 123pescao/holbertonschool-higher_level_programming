a4e9fb19aa66116c1cc756ab9ba647031f6dfac4
Joel Caban <joel.caban2017@gmail.com>
2024-05-15 14:22:18 -0400

    divides elements

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            div_result = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            div_result = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            div_result = 0
+        except IndexError:
+            print("out of range")
+            div_result = 0
+        finally:
+            result.append(div_result)
+    return result
+
+
+if __name__ == "__main__":
+    list_division = __import__('4-list_division').list_division
+
+    my_l_1 = [10, 8, 4]
+    my_l_2 = [2, 4, 4]
+    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+    print(result)
+
+    print("--")
+
+    my_l_1 = [10, 8, 4, 4]
+    my_l_2 = [2, 0, "H", 2, 7]
+    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+    print(result)
