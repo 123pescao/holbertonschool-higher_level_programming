e65ba24c8a818491781475f23435d7e637f73be9
Joel Caban <joel.caban2017@gmail.com>
2024-05-25 09:16:38 -0400

    task 03

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+class CountedIterator:
+    """Iterator that counts the number of items iterated over."""
+
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.count += 1
+        return next(self.iterator)
+
+    def get_count(self):
+        """Return the number of items iterated over."""
+        return self.count
