65c7ac6efbf26c0a16b8cdaf4e115e3324a60db3
Joel Caban <joel.caban2017@gmail.com>
2024-05-28 12:07:00 -0400

    Task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module that defines the Student class
+"""
+
+class Student:
+    """
+    Defines a student
+    """
+
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+        """
+
+
+        if attrs is None:
+            return self.__dict__
+        return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
