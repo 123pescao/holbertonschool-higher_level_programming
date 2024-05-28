fa8921a449b47edc080844cd1f7ba77343b9c2db
Joel Caban <joel.caban2017@gmail.com>
2024-05-28 10:42:02 -0400

    fixing lentgh and spacing

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,9 +3,11 @@
 Module that contains the write_file function
 """
 
+
 def write_file(filename="", text=""):
     """
-    Writes a string to a text file (UTF8) and returns the number of characters written.
+    Writes a string to a text file (UTF8) and returns
+    the number of characters written.
     """
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
