8f70ad4e247f92215fbf358401dd7d0babccb707
Joel Caban <joel.caban2017@gmail.com>
2024-05-29 11:51:31 -0400

    Task 1

---
+++ python-serialization/task_01_pickel.py
@@ -0,0 +1,28 @@
+import pickle
+
+
+class CustomObject:
+    """Custom object for demonstrating pickling."""
+
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Display the object's attributes."""
+        print(f"Name: {self.name}\nAge: {self.age}\nIs Student: {self.is_student}")
+
+    def serialize(self, filename):
+        """Serialize the object to a file using pickle."""
+        with open(filename, 'wb') as file:
+            pickle.dump(self, file)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Deserialize an object from a file using pickle."""
+        try:
+            with open(filename, 'rb') as file:
+                return pickle.load(file)
+        except (FileNotFoundError, pickle.PickleError):
+            return None
