5beec9204d2e802853d2846fae76b51d58cf3ea6
Joel Caban <joel.caban2017@gmail.com>
2024-06-22 13:17:48 -0400

    Output & pycode

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -8,19 +8,15 @@ import sys
 
 
 if __name__ == "__main__":
-    try:
-        engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'.format(
-                sys.argv[1], sys.argv[2], sys.argv[3]
-            ), pool_pre_ping=True
-        )
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        
-        cities = session.query(City).join(State).order_by(City.id).all()
-        for city in cities:
-            print("{}: ({}) {}".format(city.state.name, city.id, city.name))
-        session.close()
-    except Exception as e:
-        print("Error: {}".format(e))
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ), pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    cities = session.query(City).join(State).order_by(City.id).all()
+    for city in cities:
+        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
+    session.close()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Contains the class definition of a City"""
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from model_state import Base
 
 
@@ -12,3 +13,4 @@ class City(Base):
     )
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship("State")
