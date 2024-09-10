7988a3ce783e7b7cd7a3f1710723697831783f90
Joel <joel.caban2017@gmail.com>
2024-09-09 21:29:54 -0400

    Tasks 2-8

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for m in range(len(matrix)):
+        for n in range(len(matrix[m])):
+            if n < len(matrix[m]) - 1:
+                print("{:d} ".format(matrix[m][n]), end="")
+            else:
+                print("{:d}".format(matrix[m][n]), end="")
+        print()
+        
