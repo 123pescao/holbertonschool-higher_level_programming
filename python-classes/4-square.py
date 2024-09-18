c634c1173951d545eeea4aad12c1b11c538eee99
Joel <joel.caban2017@gmail.com>
2024-09-18 08:46:58 -0400

    Tasks 4-6

---
+++ python-classes/4-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""The module creates a class Square"""
+
+
+class Square:
+    """
+    This is the class named Square
+
+    Attributes:
+    attr1 (size): means size of square
+    """
+    def __init__(self, size=0):
+        """
+        Args:
+        size (int): size for __size attribute of class instance
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area based on size of square
+
+        Returns: an int which is the value
+        of the area
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """
+        Property to retrieve size
+
+        Return: the size of the area
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Property to set size
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
