eaab8012a454054bf2278d36ab2a2d83c9de9e23
Joel <joel.caban2017@gmail.com>
2024-10-02 15:05:39 -0400

    Task 0 (Docstring)

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""
+This module contains a function to read a file and print contents
+"""
 
 def read_file(filename=""):
-    with open("filename", encoding="utf-8") as f:
-        f.read()
+    with open(filename, encoding="utf-8") as f:
         print(f.read())
