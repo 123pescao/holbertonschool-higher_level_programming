91540c698afe7f4954e903f80f102a2878735044
Joel <joel.caban2017@gmail.com>
2024-10-02 15:10:36 -0400

    Task 0- end=

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ def read_file(filename=""):
     Reads an text file and prints its contents
     """
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
