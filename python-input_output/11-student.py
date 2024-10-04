b99b6cff43d1f05c50e18141873c9f0b64788939
Joel <joel.caban2017@gmail.com>
2024-10-03 21:12:54 -0400

    Task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Student module
+"""
+
+
+class Student:
+    """Contains student data
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary of Student with conditions to filter
+        """
+        if attrs is None or not isinstance(attrs, list):
+            return self.__dict__
+        else:
+            temp = {}
+            for elem in attrs:
+                if not isinstance(elem, str):
+                    return self.__dict__
+                if elem in self.__dict__:
+                    temp[elem] = self.__dict__[elem]
+            return temp
+
+    def reload_from_json(self, json):
+        """Replaces all items in `json`
+        """
+        for key in json.keys():
+            self.__dict__[key] = json[key]
