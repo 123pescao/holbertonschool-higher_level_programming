4b37d4a28e8894a2bd3c974b3b8b9bc1154ed214
Joel <joel.caban2017@gmail.com>
2024-10-22 15:24:17 -0400

    Tasks 15-16

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,2 @@
+-- Script to list number of records by score
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- Script to list all records where name is not NULL
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
